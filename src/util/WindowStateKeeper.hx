dd9f39c292d60f9d04257e9adf2cb4672e4c9362
austin <eastin93@gmail.com>
2019-05-19 19:01:08 -0400

    persist app window state

---
+++ src/util/WindowStateKeeper.hx
@@ -0,0 +1,33 @@
+package util;
+
+import electron.main.BrowserWindow;
+
+typedef WindowStateKeeperOptions =
+{
+  ?defaultWidth:Int,
+  ?defaultHeight:Int,
+  ?path:String,
+  ?file:String,
+  ?maximize:Bool,
+  ?fullscreen:Bool
+} 
+
+@:jsRequire('electron-window-state')
+extern class WindowStateKeeper
+{
+  public var x:Int;
+  public var y:Int;
+  public var width:Int;
+  public var height:Int;
+  public var isMaximized:Bool;
+  public var isFullscreen:Bool;
+
+  @:selfCall
+  public static function create(?options:WindowStateKeeperOptions):WindowStateKeeper;
+
+  public function manage(window:BrowserWindow):Void;
+
+  public function unmanage():Void;
+
+  public function saveState(window:BrowserWindow):Void;
+}
