1a93de560e7878ee7df0ee5c9e54c63eb8c4bce1
NemoStein <contato@nemostein.com.br>
2020-10-12 22:04:56 -0300

    Added simple autotile support in pencil tool

--- src/project/data/Tileset.hx
+++ src/project/data/Tileset.hx
@@ -1,5 +1,6 @@
 package project.data;
 
+import modules.tiles.TileLayer.TileData;
 import io.FileSystem;
 import js.Browser;
 import js.node.Path;
@@ -24,6 +25,8 @@ class Tileset
 	public var tileSeparationY: Int;
 	public var tileMarginX: Int;
 	public var tileMarginY: Int;
+
+	public var autotile: Autotile;
 
 	public var brokenPath:Bool = false;
 	public var brokenTexture:Bool = false;
@@ -54,6 +57,11 @@ class Tileset
 			brokenPath = true;
 			brokenTexture = true;
 			texture = Texture.fromString("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAsklEQVRYhcVXQQ6AIAwrPmlv8Zm8xS/NgyERAgpj0CUkJtK14mAlAFAQ4wCAKLKd+M2pADSKaHpePQqu5osd5LmA1SIaubsnriCvC/AW8ZPLDPQg/xYwK6IT65bIinFPOCrY96sMq+W3tMZ6GQZUiSaK1QTKCGd2SkgqLJE62nld1hRPO2YH9ReYBFCLkLoNqQcR9SimNiNqO6YaEqolo5pSqi2nXkyoV7Od5KWIKT/gETfAGp5SxRHyngAAAABJRU5ErkJggg==");
+		}
+
+		if (tileAuto)
+		{
+			autotile = new Autotile();
 		}
 	}
 
@@ -101,4 +109,54 @@ class Tileset
 	inline function get_tileColumns():Int return Math.floor((width - tileSeparationX - tileMarginX - tileMarginX) / (tileWidth + tileSeparationX));
 
 	inline function get_tileRows():Int return Math.floor((height - tileSeparationY - tileMarginY - tileMarginY) / (tileHeight + tileSeparationY));
+}
+
+class Autotile
+{
+	public static final X0_Y0: Int = 1 << 0;
+	public static final X1_Y0: Int = 1 << 1;
+	public static final X2_Y0: Int = 1 << 2;
+	public static final X0_Y1: Int = 1 << 3;
+	public static final X2_Y1: Int = 1 << 4;
+	public static final X0_Y2: Int = 1 << 5;
+	public static final X1_Y2: Int = 1 << 6;
+	public static final X2_Y2: Int = 1 << 7;
+
+	private var autotileMapping: Map<Int, Int> = [];
+
+	public function new()
+	{
+		var tile = 0;
+		for (i in 0...256)
+		{
+			if (!(
+				((~i & X1_Y0) != 0 && ((i & X0_Y0) != 0 || (i & X2_Y0) != 0)) ||
+				((~i & X0_Y1) != 0 && ((i & X0_Y0) != 0 || (i & X0_Y2) != 0)) ||
+				((~i & X2_Y1) != 0 && ((i & X2_Y0) != 0 || (i & X2_Y2) != 0)) ||
+				((~i & X1_Y2) != 0 && ((i & X0_Y2) != 0 || (i & X2_Y2) != 0))
+			))
+			{
+				autotileMapping[i] = tile++;
+			}
+		}
+	}
+
+	public function getTileId(x:Int, y:Int, data:Array<Array<TileData>>):Int
+	{
+		final is = (x, y) -> data[x][y].idx != -1;
+
+		var mask = 0;
+
+		if (is(x, y + 1)) mask += X1_Y2;
+		if (is(x, y - 1)) mask += X1_Y0;
+		if (is(x - 1, y)) mask += X0_Y1;
+		if (is(x + 1, y)) mask += X2_Y1;
+
+		if ((mask & (X1_Y2 | X0_Y1)) == (X1_Y2 | X0_Y1) && is(x - 1, y + 1)) mask += X0_Y2;
+		if ((mask & (X1_Y2 | X2_Y1)) == (X1_Y2 | X2_Y1) && is(x + 1, y + 1)) mask += X2_Y2;
+		if ((mask & (X1_Y0 | X0_Y1)) == (X1_Y0 | X0_Y1) && is(x - 1, y - 1)) mask += X0_Y0;
+		if ((mask & (X1_Y0 | X2_Y1)) == (X1_Y0 | X2_Y1) && is(x + 1, y - 1)) mask += X2_Y0;
+
+		return autotileMapping[mask];
+	}
 }
